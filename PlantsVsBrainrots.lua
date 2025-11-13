1ae27e0d82c256126dfa8d35266814bef7f174b5
terist999 <xscammer25@gmail.com>
2025-11-13 18:52:09 +0800

    Update PlantsVsBrainrots.lua

--- PlantsVsBrainrots.lua
+++ PlantsVsBrainrots.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://pastefy.app/xRLfUijs/raw"))()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/nouralddin-abdullah/ToastyHub-XD/refs/heads/main/hub-main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/collectorhenfi-wq/Hendarscriptt/refs/heads/main/Plantvsbrainrotupdate.txt"))()
